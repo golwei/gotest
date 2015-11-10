197a5fcd9ee0a52abf135ba3b3bb9541370feeec
golwei <lizhiweimail@qq.com>
2015-11-10 15:42:10 +0800

    Update Q6.go

--- src/Q123/Q6.go
+++ src/Q123/Q6.go
@@ -10,15 +10,7 @@ func main() {
 	fmt.Println(a, b)
 }
 
-func f1(a []float64) (avg float64) {
-	sum := 0.0
-	for _, v := range a {
-		sum += v
 
-	}
-	avg = sum / float64(len(a))
-	return
-}
 func f2(a, b int) (c, d int) {
 	if a < b {
 		c, d = a, b
